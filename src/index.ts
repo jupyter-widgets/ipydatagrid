1fec07a901d38846a48dfaf227887f200270ab39
martinRenou <martin.renou@gmail.com>
2019-07-23 11:20:09 +0200

    Fix JupyterLab build issue

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 // Copyright (c) QuantStack
 // Distributed under the terms of the Modified BSD License.
 
+// In case of classic Jupyter Notebook and embed, we provide the PhosphorJS CSS
+import '@phosphor/default-theme/style/index.css';
+
 export * from './version';
 export * from './datagrid';
