c63e5bd058bf2bbe654fb61508df0b8225f3390f
martinRenou <martin.renou@gmail.com>
2019-08-23 16:24:21 +0200

    Move Vega widget implementation in separate file

--- src/cellrenderer.ts
+++ src/cellrenderer.ts
@@ -3,9 +3,6 @@
 
 import * as _ from 'underscore';
 
-const vega_expressions: any = require('vega-expression');
-const vega_functions: any = require('vega-functions');
-
 import {
   CellRenderer, TextRenderer
 } from '@phosphor/datagrid';
@@ -22,70 +19,12 @@ import {
   BarRenderer
 } from './core/barrenderer';
 
+import {
+  VegaExprView
+} from './vegaexpr';
+
 // Temporary, will be removed when the scales are exported from bqplot
 type Scale = any;
-
-
-export
-class VegaExprModel extends WidgetModel {
-  defaults() {
-    return {...super.defaults(),
-      _model_name: VegaExprModel.model_name,
-      _model_module: VegaExprModel.model_module,
-      _model_module_version: VegaExprModel.model_module_version,
-      value: 'default_value'
-    };
-  }
-
-  initialize(attributes: any, options: any) {
-    super.initialize(attributes, options);
-
-    const codegen_params = {
-      whitelist: ['cell', 'default_value', 'functions'],
-      globalvar: 'cell',
-      functions: function(codegen: any) {
-        const fn = vega_expressions.functions(codegen);
-        for (let name in vega_functions.functionContext) { fn[name] = 'functions.' + name; }
-        return fn;
-      }
-    };
-
-    this._codegen = vega_expressions.codegen(codegen_params);
-
-    this.update_function();
-    this.on('change:value', this.update_function.bind(this));
-  }
-
-  process(config: CellRenderer.ICellConfig, default_value: any) {
-    return this._function(config, default_value, vega_functions.functionContext);
-  }
-
-  update_function() {
-    const parsed_value = this._codegen(vega_expressions.parse(this.get('value')));
-
-    this._function = Function('cell', 'default_value', 'functions', '"use strict";return(' + parsed_value.code + ')');
-  }
-
-  static model_name = 'VegaExprModel';
-  static model_module = MODULE_NAME;
-  static model_module_version = MODULE_VERSION
-  static view_name = 'VegaExprView';
-  static view_module = MODULE_NAME;
-  static view_module_version = MODULE_VERSION;
-
-  _codegen: any;
-  _function: any;
-}
-
-export
-class VegaExprView extends WidgetView {
-  process(config: CellRenderer.ICellConfig, default_value: any) {
-    return this.model.process(config, default_value);
-  }
-
-  model: VegaExprModel;
-}
-
 
 type Processor = boolean | string | number | VegaExprView | Scale;
 
