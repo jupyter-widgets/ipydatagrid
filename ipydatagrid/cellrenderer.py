a98e1ffd22ee6d3fcf2c3a5c5230fafa92ee8bc3
martinRenou <martin.renou@gmail.com>
2019-09-09 09:25:56 +0200

    Update to py2vega 0.5

--- ipydatagrid/cellrenderer.py
+++ ipydatagrid/cellrenderer.py
@@ -18,7 +18,7 @@ from ipywidgets import Widget, widget_serialization, Color
 from bqplot import Scale, ColorScale
 
 from ._frontend import module_name, module_version
-from py2vega import py2vega
+from py2vega import py2vega, Variable
 
 
 class VegaExpr(Widget):
@@ -40,7 +40,7 @@ class Expr(VegaExpr):
 
     @validate('value')
     def _validate_value(self, proposal):
-        return py2vega(proposal['value'], ['value', 'default_value', 'x', 'y', 'height', 'width', 'row', 'column'])
+        return py2vega(proposal['value'], [Variable('cell', ['value', 'row', 'column']), 'default_value'])
 
 
 class CellRenderer(Widget):
