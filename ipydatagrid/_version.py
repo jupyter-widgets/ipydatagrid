48c5ed243e6b3a4cebb364fc8fa32c25dd05dff8
Kaia Young <kyoung109@bloomberg.net>
2020-07-31 09:59:18 -0700

    Update _version.py

--- ipydatagrid/_version.py
+++ ipydatagrid/_version.py
@@ -4,5 +4,5 @@
 # Copyright (c) QuantStack.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 2, 5, 'dev')
+version_info = (0, 2, 6, 'dev')
 __version__ = ".".join(map(str, version_info))
