730c5e45b7535b51f87263f4b1b77dcd159a19c6
kaiayoung <kyoung109@bloomberg.net>
2020-09-02 14:52:03 -0700

    0.2.9

--- ipydatagrid/_version.py
+++ ipydatagrid/_version.py
@@ -4,5 +4,5 @@
 # Copyright (c) QuantStack.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 2, 8, 'dev')
+version_info = (0, 2, 9, 'dev')
 __version__ = ".".join(map(str, version_info))
