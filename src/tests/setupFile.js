461541aa54cabd32231a3c5a0a0009b818bd73dd
kaiayoung <kyoung109@bloomberg.net>
2020-09-23 16:55:24 -0700

    Add filter menu tests

--- src/tests/setupFile.js
+++ src/tests/setupFile.js
@@ -1,4 +1,13 @@
 // jsdom does not support HTML5 canvas, so we need to mock out this function
 HTMLCanvasElement.prototype.getContext = () => { 
-  return {}
+  return {
+    scale: () => {},
+    fillRect: () => {},
+    strokeRect: () => {},
+    fillText: () => {},
+    beginPath: () => {},
+    moveTo: () => {},
+    lineTo: () => {},
+    stroke: () => {}
+  }
 };
