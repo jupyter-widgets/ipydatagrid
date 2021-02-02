ca3b0c727cbe7ad0a81f95e318d367cce3ae1010
Itay Dafna <i.b.dafna@gmail.com>
2021-02-02 11:37:32 -0800

    fix version imports

--- ipydatagrid/__init__.py
+++ ipydatagrid/__init__.py
@@ -3,7 +3,7 @@
 
 from .cellrenderer import CellRenderer, TextRenderer, BarRenderer, VegaExpr, Expr  # noqa
 from .datagrid import DataGrid, SelectionHelper  # noqa
-from ._version import __version__, version_info  # noqa
+from ._version import __version__  # noqa
 
 def _jupyter_nbextension_paths():
     return [{
--- ipydatagrid/_frontend.py
+++ ipydatagrid/_frontend.py
@@ -5,7 +5,7 @@
 Information about the frontend package of the widgets.
 """
 
-from ._version import version_info
+from ._version import __version__
 
 module_name = "ipydatagrid"
-module_version = "^{0}.{1}.{2}".format(version_info[0], version_info[1], version_info[2])
+module_version = "^{}".format(__version__)
