812dec1d976435bfbfae481ceec064888763de65
Itay Dafna <i.b.dafna@gmail.com>
2021-02-01 21:29:49 -0800

    version: switch from variable to string

--- ipydatagrid/_version.py
+++ ipydatagrid/_version.py
@@ -1,5 +1,4 @@
 # Copyright (c) Bloomberg.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 2, 14, 'dev')
-__version__ = ".".join(map(str, version_info))
+__version__ = "0.2.14dev"
