50e89beb0b94b960711aa27285bedb4db42d50df
Itay Dafna <i.b.dafna@gmail.com>
2021-04-30 12:35:06 -0700

    Converge on a solution and update tests
    Signed-off-by: Itay Dafna <i.b.dafna@gmail.com>

--- js/datagrid.ts
+++ js/datagrid.ts
@@ -135,18 +135,14 @@ export class DataGridModel extends DOMWidgetModel {
       // Need to update existing ViewBasedJSONModel's dataset attribute
       // before discarding.
       this.data_model.updateDataset({ data: data.data, schema: schema });
-      // Those two cause tests to fail..:
-      //
-      // this.data_model.transformStateChanged.disconnect(this.syncTransformState);
-      // this.data_model.dataSync.disconnect(this.updateDataSync);
+    } else {
+      this.data_model = new ViewBasedJSONModel({
+        data: data.data,
+        schema: schema,
+      });
+      this.data_model.transformStateChanged.connect(this.syncTransformState);
+      this.data_model.dataSync.connect(this.updateDataSync);
     }
-
-    this.data_model = new ViewBasedJSONModel({
-      data: data.data,
-      schema: schema,
-    });
-    this.data_model.transformStateChanged.connect(this.syncTransformState);
-    this.data_model.dataSync.connect(this.updateDataSync);
 
     this.updateTransforms();
     this.trigger('data-model-changed');
