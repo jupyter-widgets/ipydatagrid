0b3149c5b250a9f920b67504ab850fa49db8be4f
Itay Dafna <i.b.dafna@gmail.com>
2021-06-22 21:27:53 -0700

    Update headerRenderer with latest features
    Signed-off-by: Itay Dafna <i.b.dafna@gmail.com>

--- js/core/headerRenderer.ts
+++ js/core/headerRenderer.ts
@@ -51,7 +51,7 @@ export class HeaderRenderer extends TextRenderer {
 
     // Format the cell value to text.
     const format = this.format;
-    const text = format(config);
+    let text = format(config);
 
     // Bail if there is no text to draw.
     if (!text) {
@@ -61,6 +61,15 @@ export class HeaderRenderer extends TextRenderer {
     // Resolve the vertical and horizontal alignment.
     const vAlign = CellRenderer.resolveOption(this.verticalAlignment, config);
     const hAlign = CellRenderer.resolveOption(this.horizontalAlignment, config);
+
+    // Resolve the elision direction
+    const elideDirection = CellRenderer.resolveOption(
+      this.elideDirection,
+      config,
+    );
+
+    // Resolve the text wrapping flag
+    const wrapText = CellRenderer.resolveOption(this.wrapText, config);
 
     // Compute the padded text box height for the specified alignment.
     const boxHeight = config.height - (vAlign === 'center' ? 1 : 2);
@@ -76,6 +85,7 @@ export class HeaderRenderer extends TextRenderer {
     // Set up the text position variables.
     let textX: number;
     let textY: number;
+    let boxWidth: number;
 
     // Compute the Y position for the text.
     switch (vAlign) {
@@ -95,13 +105,16 @@ export class HeaderRenderer extends TextRenderer {
     // Compute the X position for the text.
     switch (hAlign) {
       case 'left':
-        textX = config.x + 2;
+        textX = config.x + 8;
+        boxWidth = config.width - 14;
         break;
       case 'center':
         textX = config.x + config.width / 2;
+        boxWidth = config.width;
         break;
       case 'right':
-        textX = config.x + config.width - 3;
+        textX = config.x + config.width - 8;
+        boxWidth = config.width - 14;
         break;
       default:
         throw 'unreachable';
@@ -120,7 +133,117 @@ export class HeaderRenderer extends TextRenderer {
     gc.textAlign = hAlign;
     gc.textBaseline = 'bottom';
 
-    // Draw the text
+    // The current text width in pixels.
+    let textWidth = gc.measureText(text).width;
+
+    // Apply text wrapping if enabled.
+    if (wrapText && textWidth > boxWidth) {
+      // Make sure box clipping happens.
+      gc.beginPath();
+      gc.rect(config.x, config.y, config.width, config.height - 1);
+      gc.clip();
+
+      // Split column name to words based on
+      // whitespace preceding a word boundary.
+      // "Hello  world" --> ["Hello  ", "world"]
+      const wordsInColumn = text.split(/\s(?=\b)/);
+
+      // Y-coordinate offset for any additional lines
+      let curY = textY;
+      let textInCurrentLine = wordsInColumn.shift()!;
+
+      // Single word. Applying text wrap on word by splitting
+      // it into characters and fitting the maximum number of
+      // characters possible per line (box width).
+      if (wordsInColumn.length === 0) {
+        let curLineTextWidth = gc.measureText(textInCurrentLine).width;
+        while (curLineTextWidth > boxWidth && textInCurrentLine !== '') {
+          // Iterating from the end of the string until we find a
+          // substring (0,i) which has a width less than the box width.
+          for (let i = textInCurrentLine.length; i > 0; i--) {
+            const curSubString = textInCurrentLine.substring(0, i);
+            const curSubStringWidth = gc.measureText(curSubString).width;
+            if (curSubStringWidth < boxWidth || curSubString.length === 1) {
+              // Found a substring which has a width less than the current
+              // box width. Rendering that substring on the current line
+              // and setting the remainder of the parent string as the next
+              // string to iterate on for the next line.
+              const nextLineText = textInCurrentLine.substring(
+                i,
+                textInCurrentLine.length,
+              );
+              textInCurrentLine = nextLineText;
+              curLineTextWidth = gc.measureText(textInCurrentLine).width;
+              gc.fillText(curSubString, textX, curY);
+              curY += textHeight;
+              // No need to continue iterating after we identified
+              // an index to break the string on.
+              break;
+            }
+          }
+        }
+      }
+
+      // Multiple words in column header. Fitting maximum
+      // number of words possible per line (box width).
+      else {
+        while (wordsInColumn.length !== 0) {
+          // Processing the next word in the queue.
+          const curWord = wordsInColumn.shift();
+          // Joining that word with the existing text for
+          // the current line.
+          const incrementedText = [textInCurrentLine, curWord].join(' ');
+          const incrementedTextWidth = gc.measureText(incrementedText).width;
+          if (incrementedTextWidth > boxWidth) {
+            // If the newly combined text has a width larger than
+            // the box width, we render the line before the current
+            // word was added. We set the current word as the next
+            // line.
+            gc.fillText(textInCurrentLine, textX, curY);
+            curY += textHeight;
+            textInCurrentLine = curWord!;
+          } else {
+            // The combined text hasd a width less than the box width. We
+            // set the the current line text to be the new combined text.
+            textInCurrentLine = incrementedText;
+          }
+        }
+      }
+      gc.fillText(textInCurrentLine!, textX, curY);
+      // Terminating the call here as we don't want
+      // to apply text eliding when wrapping is active.
+      return;
+    }
+
+    // Elide text that is too long
+    const elide = '\u2026';
+
+    // Compute elided text
+    if (elideDirection === 'right') {
+      while (textWidth > boxWidth && text.length > 1) {
+        if (text.length > 4 && textWidth >= 2 * boxWidth) {
+          // If text width is substantially bigger, take half the string
+          text = text.substring(0, text.length / 2 + 1) + elide;
+        } else {
+          // Otherwise incrementally remove the last character
+          text = text.substring(0, text.length - 2) + elide;
+        }
+        textWidth = gc.measureText(text).width;
+      }
+    } else {
+      while (textWidth > boxWidth && text.length > 1) {
+        if (text.length > 4 && textWidth >= 2 * boxWidth) {
+          // If text width is substantially bigger, take half the string
+          text = elide + text.substring(text.length / 2);
+        } else {
+          // Otherwise incrementally remove the last character
+          text = elide + text.substring(2);
+        }
+        textWidth = gc.measureText(text).width;
+      }
+    }
+
+    // Draw the text for the cell.
     gc.fillText(text, textX, textY);
 
     // Check if not bottom row of 'column-header' CellRegion
