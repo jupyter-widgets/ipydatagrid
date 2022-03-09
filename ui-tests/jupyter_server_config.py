02138ce7071cc9719585f1717abe028d489b827f
Itay Dafna <idafna@seas.upenn.edu>
2022-03-09 08:52:02 -0800

    Apply linting
    Signed-off-by: Itay Dafna <idafna@seas.upenn.edu>

--- ui-tests/jupyter_server_config.py
+++ ui-tests/jupyter_server_config.py
@@ -1,10 +1,10 @@
 from tempfile import mkdtemp
 
-c.ServerApp.port = 8888
-c.ServerApp.token = ""
-c.ServerApp.password = ""
-c.ServerApp.disable_check_xsrf = True
-c.ServerApp.open_browser = False
-c.ServerApp.root_dir = mkdtemp(prefix='galata-test-')
+c.ServerApp.port = 8888  # noqa: F821
+c.ServerApp.token = ""  # noqa: F821
+c.ServerApp.password = ""  # noqa: F821
+c.ServerApp.disable_check_xsrf = True  # noqa: F821
+c.ServerApp.open_browser = False  # noqa: F821
+c.ServerApp.root_dir = mkdtemp(prefix="galata-test-")  # noqa: F821
 
-c.LabApp.expose_app_in_browser = True
+c.LabApp.expose_app_in_browser = True  # noqa: F821
