e90475ea64f12336de6602c8e34fe4acba0081b2
Itay Dafna <i.b.dafna@gmail.com>
2022-09-28 18:20:46 -0700

    Update venv names for split testing
    Signed-off-by: Itay Dafna <i.b.dafna@gmail.com>

---
+++ ui-tests-ipw8/tests/ipydatagrid.test.ts
@@ -0,0 +1,129 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+
+import { IJupyterLabPageFixture, test } from '@jupyterlab/galata';
+import { expect } from '@playwright/test';
+import * as path from 'path';
+const klaw = require('klaw-sync');
+
+
+const filterUpdateNotebooks = item => {
+  const basename = path.basename(item.path);
+  return basename.includes('_update');
+}
+
+const testCellOutputs = async (page: IJupyterLabPageFixture, tmpPath: string, theme: 'JupyterLab Light' | 'JupyterLab Dark') => {
+  const paths = klaw(path.resolve(__dirname, './notebooks'), { filter: item => !filterUpdateNotebooks(item), nodir: true });
+  const notebooks = paths.map(item => path.basename(item.path));
+
+  const contextPrefix = theme == 'JupyterLab Light' ? 'light_' : 'dark_';
+  page.theme.setTheme(theme);
+
+  for (const notebook of notebooks) {
+    let results = [];
+
+    await page.notebook.openByPath(`${tmpPath}/${notebook}`);
+    await page.notebook.activate(notebook);
+
+    let numCellImages = 0;
+
+    const getCaptureImageName = (contextPrefix: string, notebook: string, id: number): string => {
+      return `${contextPrefix}-${notebook}-cell-${id}.png`;
+    };
+
+    await page.notebook.runCellByCell({
+      onAfterCellRun: async (cellIndex: number) => {
+        const cell = await page.notebook.getCellOutput(cellIndex);
+        if (cell) {
+          results.push(await cell.screenshot());
+          numCellImages++;
+        }
+      }
+    });
+
+    await page.notebook.save();
+
+    for (let c = 0; c < numCellImages; ++c) {
+      expect(results[c]).toMatchSnapshot(getCaptureImageName(contextPrefix, notebook, c));
+    }
+
+    await page.notebook.close(true);
+  }
+}
+
+const testPlotUpdates = async (page: IJupyterLabPageFixture, tmpPath: string, theme: 'JupyterLab Light' | 'JupyterLab Dark') => {
+  const paths = klaw(path.resolve(__dirname, './notebooks'), { filter: item => filterUpdateNotebooks(item), nodir: true });
+  const notebooks = paths.map(item => path.basename(item.path));
+
+  const contextPrefix = theme == 'JupyterLab Light' ? 'light_' : 'dark_';
+  page.theme.setTheme(theme);
+
+  for (const notebook of notebooks) {
+    let results = [];
+
+    await page.notebook.openByPath(`${tmpPath}/${notebook}`);
+    await page.notebook.activate(notebook);
+
+    const getCaptureImageName = (contextPrefix: string, notebook: string, id: number): string => {
+      return `${contextPrefix}-${notebook}-cell-${id}.png`;
+    };
+
+    let cellCount = 0;
+    await page.notebook.runCellByCell({
+      onAfterCellRun: async (cellIndex: number) => {
+        // Always get first cell output which must contain the plot
+        const cell = await page.notebook.getCellOutput(0);
+        if (cell) {
+          results.push(await cell.screenshot());
+          cellCount++;
+        }
+      }
+    });
+
+    await page.notebook.save();
+
+    for (let i = 0; i < cellCount; i++) {
+      expect(results[i]).toMatchSnapshot(getCaptureImageName(contextPrefix, notebook, i));
+    }
+
+    await page.notebook.close(true);
+  }
+};
+
+test.describe('ipydatagrid Visual Regression', () => {
+  test.beforeEach(async ({ page, tmpPath }) => {
+    await page.contents.uploadDirectory(
+      path.resolve(__dirname, './notebooks'),
+      tmpPath
+    );
+    await page.filebrowser.openDirectory(tmpPath);
+  });
+
+  test('Light theme: Check ipydatagrid first renders', async ({
+    page,
+    tmpPath,
+  }) => {
+    await testCellOutputs(page, tmpPath, 'JupyterLab Light');
+  });
+
+  test('Dark theme: Check ipydatagrid first renders', async ({
+    page,
+    tmpPath,
+  }) => {
+    await testCellOutputs(page, tmpPath, 'JupyterLab Dark');
+  });
+
+  test('Light theme: Check ipydatagrid update plot properties', async ({
+    page,
+    tmpPath,
+  }) => {
+    await testPlotUpdates(page, tmpPath, 'JupyterLab Light');
+  });
+
+  test('Dark theme: Check ipydatagrid update plot properties', async ({
+    page,
+    tmpPath,
+  }) => {
+    await testPlotUpdates(page, tmpPath, 'JupyterLab Dark');
+  });
+});
